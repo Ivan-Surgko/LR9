8d2e68a5f239ec4213ad29270c753a3cd4a6691e
Ivan-Surgko <asus.vre@gmail.com>
2025-01-30 00:58:02 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-//підключення модулів
-let express=require('express');
-app-express(); //
-let server=require('http').createServer(app);
-let io=require('socket.io') (server);
+// Підключення модулів
+let express = require('express');
+let app = express(); // ❌ Було: app-express(); ✅ Стало: app = express();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
 
-
-// прослуховування порту 
+// Прослуховування порту
 server.listen(5000);
 
-//відслідкування url адреси та завантаження вмісту файлу html 
+// Відслідковування URL-адреси та завантаження файлу index.html
 app.get('/', function(request, response) {
-      response.sendFile(_dirname + '/index.html'); 
-    })
-
-// створення масивів для користувачів та підключень 
-users=[];
-connections=[];
-
-// функція, яка спрацьовує, коли користувач підключається до чату
-lo.sockets.on('connection', function(socket) {
-     console.log('Підключення');
-     connections.push(socket);
-
-socket.on('disconnect', function(data){
-    
-    connections.splice(connections.indexOf(socket), 1);
-    console.log('Відключено');
+    response.sendFile(__dirname + '/index.html'); // ❌ Було: _dirname ✅ Стало: __dirname
 });
 
-// функція, яка отримує ім'я та повідомлення від користувача 
-socket.on('send mess', function(data) {
-io.sockets.emit('add mess', {mess: data.mess, name:data.name});
-});
+// Масиви для користувачів та підключень
+let users = [];
+let connections = [];
+
+// Функція, яка виконується при підключенні користувача
+io.sockets.on('connection', function(socket) { // ❌ Було: lo.sockets ✅ Стало: io.sockets
+    console.log('Користувач підключився');
+    connections.push(socket);
+
+    // Відключення користувача
+    socket.on('disconnect', function() {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Користувач відключився');
+    });
+
+    // Отримання повідомлення від користувача
+    socket.on('send mess', function(data) {
+        io.sockets.emit('add mess', { mess: data.mess, name: data.name });
+    });
 });
